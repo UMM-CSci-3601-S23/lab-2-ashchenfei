ce1a700249a4f68b7591c2b8c097fd74b30a6950
Ash <plase024@morris.umn.edu>
2023-01-24 14:29:32 -0600

    Add TodoDatabase Class
    Co-authored-by: Chenfei Peng <peng0368@morris.umn.edu>

---
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -0,0 +1,27 @@
+package umm3601.todo;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+// import io.javalin.http.BadRequestResponse;
+
+/*
+ * A fake "database" of the todo's loaded from file
+ */
+public class TodoDatabase {
+  private Todo[] allTodos;
+
+  public TodoDatabase(String todoDataFilename) throws IOException {
+    InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoDataFilename));
+    ObjectMapper objectMapper = new ObjectMapper();
+    allTodos = objectMapper.readValue(reader, Todo[].class);
+  }
+
+  public int size() {
+    return allTodos.length;
+  }
+}
