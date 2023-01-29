5a50bb01125e9e76baa11e2169a8a1e1593a2555
Ash <plase024@morris.umn.edu>
2023-01-29 12:57:45 -0600

    Implement filtering by owner and category

--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -3,6 +3,7 @@ package umm3601.todo;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +28,18 @@ public class TodoDatabase {
 
   public Todo[] listTodos(Map<String, List<String>> queryParams) {
     Todo[] filteredTodos = allTodos;
+
+    // Filter the todos by category
+    if (queryParams.containsKey("category")) {
+      String category = queryParams.get("category").get(0);
+      filteredTodos = filterTodosByCategory(filteredTodos, category);
+    }
+
+    // Filter the todos by owner
+    if (queryParams.containsKey("owner")) {
+      String owner = queryParams.get("owner").get(0);
+      filteredTodos = filterTodosByOwner(filteredTodos, owner);
+    }
 
     // Filter todos by if they contain a given substring
     if (queryParams.containsKey("contains")) {
@@ -80,5 +93,19 @@ public class TodoDatabase {
    */
   public Todo[] filterTodosByContainment(Todo[] todos, String value) {
     return Arrays.stream(todos).filter(x -> (x.body.contains(value))).toArray(Todo[]::new);
+  }
+
+  /**
+   * Get an array of all the todos having the target owner.
+   */
+  public Todo[] filterTodosByOwner(Todo[] todos, String owner) {
+    return Arrays.stream(todos).filter(x -> (x.owner.equals(owner))).toArray(Todo[]::new);
+  }
+
+  /**
+   * Get an array of all the todos having the target category.
+   */
+  public Todo[] filterTodosByCategory(Todo[] todos, String category) {
+    return Arrays.stream(todos).filter(x -> (x.category.equals(category))).toArray(Todo[]::new);
   }
 }
